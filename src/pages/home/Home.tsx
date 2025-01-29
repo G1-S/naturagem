0641d16d18d8c5df08a6cf359c3d97f656d6482c
mozartCuman <mozartcumana@gmail.com>
2025-01-29 12:51:01 -0300

    Metade do projeto setado e pre pronto

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
     <div 
       className="relative w-full h-screen flex flex-col justify-center items-center text-white text-center"
       style={{
-        backgroundImage: "url('/fotohome.jpeg')",
+        backgroundImage: "url('/natureba.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "left",
         backgroundRepeat: "no-repeat",
@@ -16,14 +16,14 @@ function Home() {
       <div className="absolute inset-0 bg-black/50"></div>
 
       <div className="relative z-10 max-w-4xl px-6">
-        <h1 className="text-4xl md:text-5xl font-bold hover:text-orange-500 transition">
-          We are Shape One!
+        <h1 className="text-4xl md:text-5xl font-bold text-red-100 hover:text-green-600 transition">
+          Delivery Rangers!
         </h1>
-        <p className="mt-10 text-lg">
-            <p>  Nós somos a Shape One, somos focados em acessibilidade e crescimento pessoal através de exercícios. Nosso objetivo é capacitar indivíduos de todas as idades e habilidades a alcançarem o seu potencial máximo, promovendo um estilo de vida saudável e equilibrado.</p>
-          </p>
-        <button className="mt-6 px-6 py-3 bg-orange-500 hover:bg-orange-600 text-white font-bold rounded-full shadow-lg transition">
-          Veja nossos treinos
+        <p className="mt-20 text-lg">
+            Aqui nós fazemos de tudo para lhe enviar os melhores produtos
+            </p>
+        <button className="mt-6 px-6 py-3 bg-red-800 hover:bg-green-600 text-white font-bold rounded-full shadow-lg transition">
+          Ofertas
         </button>
       </div>
     </div>
